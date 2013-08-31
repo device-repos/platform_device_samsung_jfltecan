760eaea949532f84f21f76529a5d679f9bd40489
Florent Guichard <guichardflorent@gmail.com>
2013-08-31 03:21:09 +0200

    fix typo

--- full_jfltecan.mk
+++ full_jfltecan.mk
@@ -28,7 +28,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/samsung/jfltecan/device.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_jfltevl
+PRODUCT_NAME := full_jfltecan
 PRODUCT_DEVICE := jfltecan
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
