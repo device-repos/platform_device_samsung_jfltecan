2a84ddc5e75479666004aa447e73e0e09c9ba7d3
treken <chupekevin@gmail.com>
2013-09-29 17:34:34 -0400

    Prepare for OctOS

---
+++ oct.mk
@@ -0,0 +1,15 @@
+$(call inherit-product, device/samsung/jfltecan/full_jfltecan.mk)
+
+# Inherit some common Oct stuff.
+ $(call inherit-product, vendor/oct/config/gsm.mk)
+
+# Enhanced NFC
+ $(call inherit-product, vendor/oct/config/nfc_enhanced.mk)
+
+# Inherit some common Oct stuff.
+$(call inherit-product, vendor/oct/config/common_full_phone.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltecan TARGET_DEVICE=jfltecan BUILD_FINGERPRINT="samsung/oct_jfltecan/jfltecan:4.3/JLS36G/user/release-keys" PRIVATE_BUILD_DESC="jfltecan-user 4.3 JLS36G release-keys"
+
+PRODUCT_NAME := oct_jfltecan
+PRODUCT_DEVICE := jfltecan
