0edf7d22f58ab2635ce77b6d94be43c5f50df85d
Carl Windhausen <cjwindhausen@gmail.com>
2013-11-19 22:28:03 -0500

    4.4_r1.2 KRT16S update

--- oct.mk
+++ oct.mk
@@ -9,7 +9,7 @@ $(call inherit-product, device/samsung/jfltecan/full_jfltecan.mk)
 # Inherit some common Oct stuff.
 $(call inherit-product, vendor/oct/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltecan TARGET_DEVICE=jfltecan BUILD_FINGERPRINT="samsung/oct_jfltecan/jfltecan:4.4/KRT16O/user/release-keys" PRIVATE_BUILD_DESC="jfltecan-user 4.4 KRT16O release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltecan TARGET_DEVICE=jfltecan BUILD_FINGERPRINT="samsung/oct_jfltecan/jfltecan:4.4/KRT16S/user/release-keys" PRIVATE_BUILD_DESC="jfltecan-user 4.4 KRT16S release-keys"
 
 PRODUCT_NAME := oct_jfltecan
 PRODUCT_DEVICE := jfltecan
